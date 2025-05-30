cada628e6a4ba242e2d1d669997234d9077df466
Dipak Kurmi <kurmideepak7@gmail.com>
2025-05-30 13:08:13 +0530

    set frontend base url

--- src/main/java/com/deep/chat/config/AppConstants.java
+++ src/main/java/com/deep/chat/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.deep.chat.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL = "http://localhost:5173";
+    public static final String FRONT_END_BASE_URL = "https://chat-app-frontend-mu-tawny.vercel.app/";
 }
