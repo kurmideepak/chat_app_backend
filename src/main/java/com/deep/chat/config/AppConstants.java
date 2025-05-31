a225b08c0ae842ffcb7f60910bb4c173bb647772
Dipak Kurmi <kurmideepak7@gmail.com>
2025-05-31 11:20:33 +0530

    new updated

--- src/main/java/com/deep/chat/config/AppConstants.java
+++ src/main/java/com/deep/chat/config/AppConstants.java
@@ -1,5 +1,5 @@
 package com.deep.chat.config;
 
 public class AppConstants {
-    public static final String FRONT_END_BASE_URL = "https://chat-app-frontend-mu-tawny.vercel.app/";
+    public static final String FRONT_END_BASE_URL = "https://chat-app-frontend-hb4mrtvdj-dipak-kurmis-projects-9d840a9c.vercel.app/";
 }
